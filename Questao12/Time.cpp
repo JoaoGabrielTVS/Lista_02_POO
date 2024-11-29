fd643365e238968eec7c5717e124069fb8c6ce89
JoaoGabrielTVS <151951938+JoaoGabrielTVS@users.noreply.github.com>
2024-11-29 16:54:07 -0300

    Add files via upload

---
+++ Questao12/Time.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <format>
+using std::cout, std::endl, std::format;
+
+
+#include "Time.hpp"
+
+Time::Time(int hr, int min, int sec)
+{
+  setTime(hr, min, sec);  
+  //qualquer modificação posterior em setTime reflete no construtor
+}
+
+void Time::setTime(int h, int m, int s)
+{
+  setHour(h);
+  setMinute(m);
+  setSecond(s);  
+}
+
+std::string Time::toUniversal() const
+{
+	return format("{:02d}:{:02d}:{:02d}", hour, minute, second);
+}
+
+std::string Time::toStandard() const
+{
+	return format("{:02d}:{:02d}:{:02d} {}", ( (hour == 0 || hour == 12) ? 12 : hour % 12 ), minute, second, (hour < 12 ? "AM" : "PM"));
+}
+
+void Time::Tick(){
+    if(getSecond()<59 )
+      setSecond(getSecond()+1);
+    else
+      if(getMinute() < 59)
+        setMinute(getMinute() + 1);
+      else
+        if(getHour()<23)
+          setHour(getHour()+1);
+        else
+          setHour(0);
+          setMinute(0);
+          setSecond(0);
+
+}
